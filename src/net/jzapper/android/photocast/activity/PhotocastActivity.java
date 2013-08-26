966a71de52e28d397ee078743934687c7bc2797e
Jason Chionh <jasonchionh@gmail.com>
2013-08-26 20:19:36 +0800

    implement netwrok discovering of chromecast device

--- src/net/jzapper/android/photocast/activity/PhotocastActivity.java
+++ src/net/jzapper/android/photocast/activity/PhotocastActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.google.cast.CastDevice;
 import net.jzapper.android.photocast.R;
+import net.jzapper.android.photocast.app.PhotocastApp;
+import net.jzapper.android.photocast.dialog.DeviceSelectionDialog;
 
 public class PhotocastActivity extends Activity {
 
-    private CastDevice castDevice;
+    private DeviceSelectionDialog deviceSelectionDialog;
 
     /**
      * Called when the activity is first created.
@@ -16,5 +18,43 @@ public class PhotocastActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        selectDevice();
+    }
+
+
+    /**
+     * Creates a new DeviceSelectionDialog with an attached listener, which listens for device
+     * selection, sets the device in the top-level Application, and enables the game start button.
+     */
+    private void selectDevice() {
+        deviceSelectionDialog = new DeviceSelectionDialog(this);
+        deviceSelectionDialog.setListener(new DeviceSelectionDialog.DeviceSelectionListener() {
+
+            @Override
+            public void onSelected(DeviceSelectionDialog dialog) {
+                deviceSelectionDialog = null;
+                CastDevice device = dialog.selectedDevice();
+                if (device != null) {
+                    //setConnectedDeviceTextView(dialog.selectedDevice().getFriendlyName());
+                    PhotocastApp.getInstance().setCastDevice(device);
+                    //findViewById(R.id.start).setEnabled(true);
+                } else {
+                    //setConnectedDeviceTextView(
+                    //        MainActivity.this.getResources().getString(R.string.no_device));
+                    //findViewById(R.id.start).setEnabled(false);
+                }
+            }
+
+            @Override
+            public void onCancelled(DeviceSelectionDialog dialog) {
+                deviceSelectionDialog = null;
+            }
+        });
+        try {
+            deviceSelectionDialog.show();
+        } catch (IllegalStateException e) {
+            //showErrorDialog(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
--- src/net/jzapper/android/photocast/dialog/DeviceSelectionDialog.java
+++ src/net/jzapper/android/photocast/dialog/DeviceSelectionDialog.java
@@ -2,9 +2,12 @@ package net.jzapper.android.photocast.dialog;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import com.google.cast.CastDevice;
 import com.google.cast.CastDeviceAdapter;
 import com.google.cast.DeviceManager;
+import net.jzapper.android.photocast.R;
+import net.jzapper.android.photocast.app.PhotocastApp;
 
 /**
  * User: jchionh
@@ -40,5 +43,109 @@ public class DeviceSelectionDialog {
     private DeviceSelectionListener deviceSelectionListener;
 
 
+    // ctor
+    public DeviceSelectionDialog(Context appContext) {
+        context = appContext;
+        castDeviceAdapter = new CastDeviceAdapter(context);
+        deviceManager = new DeviceManager(PhotocastApp.getInstance().getCastContext());
 
+        deviceManager.addListener(new DeviceManager.Listener() {
+            @Override
+            public void onScanStateChanged(int state) {
+                if (state == DeviceManager.SCAN_SUSPENDED_NETWORK_ERROR) {
+                    new AlertDialog.Builder(context)
+                            .setMessage(R.string.scan_failed_network_error)
+                            .setPositiveButton(R.string.ok, null)
+                            .create()
+                            .show();
+                }
+            }
+
+            @Override
+            public void onDeviceOnline(CastDevice castDevice) {
+                castDeviceAdapter.add(castDevice);
+            }
+
+            @Override
+            public void onDeviceOffline(CastDevice castDevice) {
+                castDeviceAdapter.remove(castDevice);
+            }
+        });
+    }
+
+
+    /**
+     * Initializes and displays the dialog being used for device selection.
+     *
+     * @throws IllegalStateException if the dialog is already being displayed
+     */
+    public void show() throws IllegalStateException {
+        if (alertDialog != null) {
+            throw new IllegalStateException("Can't show dialog more than once");
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.device_discover_title);
+        DialogListener listener = new DialogListener();
+        builder.setAdapter(castDeviceAdapter, listener);
+        alertDialog = builder.create();
+        alertDialog.setOnDismissListener(listener);
+        alertDialog.setOnCancelListener(listener);
+        deviceManager.startScan();
+        alertDialog.show();
+    }
+
+    /**
+     * Dismisses the dialog if it is being displayed.
+     */
+    public void dismiss() {
+        deviceSelectionListener = null;
+        if (alertDialog != null) {
+            alertDialog.dismiss();
+        }
+    }
+
+    /**
+     * Returns the device selected from the dialog, or {@code null} if none is selected.
+     */
+    public CastDevice selectedDevice() {
+        return castDevice;
+    }
+
+    public void setListener(DeviceSelectionListener listener) {
+        deviceSelectionListener = listener;
+    }
+
+    /**
+     * A Listener class attached to this object's displayed dialog, which handles dismiss, cancel,
+     * and click events by modifying instance variables accordingly.
+     */
+    private class DialogListener implements DialogInterface.OnClickListener,
+            DialogInterface.OnCancelListener, DialogInterface.OnDismissListener {
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            deviceManager.stopScan();
+            if (deviceSelectionListener != null) {
+                if (castDevice != null) {
+                    deviceSelectionListener.onSelected(DeviceSelectionDialog.this);
+                } else {
+                    deviceSelectionListener.onCancelled(DeviceSelectionDialog.this);
+                }
+            }
+            alertDialog = null;
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            castDevice = null;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int index) {
+            CastDevice selected = castDeviceAdapter.getItem(index);
+            if (selected != null) {
+                castDevice = selected;
+            }
+        }
+    }
 }
